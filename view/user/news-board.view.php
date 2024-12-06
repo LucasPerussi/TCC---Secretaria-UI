8a708eb7bc576f5cd142a563c9b77b2825e6a28d
AzureAD\LucasPerussi-Wetalki <perussilucas@hotmail.com>
2024-12-06 16:53:19 -0300

    redesign mural

--- view/user/news-board.view.php
+++ view/user/news-board.view.php
@@ -2,46 +2,100 @@
 
 use API\Controller\Config;
 use API\Controller\ipinfo;
-use const Siler\Config\CONFIG;
+use const Siler\Config\CONFIG; ?>
 
-date_default_timezone_set('America/Sao_Paulo');
-?>
 
-<?php if (!isset($_SESSION["user_id"])) : ?>
-    <html>
-        <head>
-            <meta http-equiv="Refresh" content="0; url=<?= Config::BASE_URL . "login"; ?>" />
-        </head>
-    </html>
-<?php else : ?>
 
-    <!DOCTYPE html>
-    <html lang="en" data-textdirection="ltr">
-        <head>
-            <?php include "view/src/head.php"; ?>
-            <link rel="stylesheet" href="src/css/news-board.css">
-            <title>Mural UFPR</title>
-        </head>
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
 
-        <body class="snippet-body vertical-layout vertical-menu-modern  navbar-floating footer-static dark-layout  <?php if ($_SESSION["user_id"]) {
-                                                                                                                            echo "menu-collapsed";
-                                                                                                                        } else {
-                                                                                                                            echo "menu-hide";
-                                                                                                                        } ?>" data-open="click" style="padding-right:0px;" data-menu="vertical-menu-modern">
-            <?php include "view/src/header.php"; ?>
 
-            <?php include "view/src/mainMenu.php"; ?>
+<head>
 
-            <div class="main-content">
-                <div class="content-wrapper">
-                    <div class="content-body">
-                        
-                    <h1 class="title">Mural</h1>
-                    <div class="cards-container">
-                        <?php if (!isset($murais["error"])) { ?>
+    <?php include "view/src/head.php"; ?>
+    <meta charset="utf-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    <link rel="stylesheet" type="text/css" href="layout/app-assets/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="layout/app-assets/css/pages/dashboard-ecommerce.css">
+    <link rel="stylesheet" type="text/css" href="layout/app-assets/css/plugins/extensions/ext-component-toastr.css">
+    <link rel="stylesheet" type="text/css" href="src/css/dashboard.css">
+    <link rel="stylesheet" href="src/css/main.css">
+    <meta name='robots' content='noindex'>
+    <link rel="stylesheet" type="text/css" href="src/css/swiper-bundle.min.css">
+    <link rel="stylesheet" type="text/css" href="src/css/carousel.css">
+    <title>Painel Admin</title>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap');
+
+        .card:hover {
+            transform: unset !important;
+        }
+
+        .options:hover {
+            transform: scale(1.05) !important;
+        }
+    </style>
+    <link rel="stylesheet" type="text/css" href="layout/assets/css/style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css'>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css'>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="src/css/inbound.css">
+    <?php
+    date_default_timezone_set('America/Sao_Paulo');
+    // $date = date('Y-m-d H:i');
+    $date = date('H');
+    // $date = 14;
+
+    ?>
+
+    <!--END: Head-->
+</head>
+
+<!-- BEGIN: Body style="background-color:#F4F4F8;"-->
+
+<body class="snippet-body vertical-layout vertical-menu-modern  navbar-floating footer-static dark-layout  <?php if ($_SESSION["user_id"]) {
+                                                                                                                echo "menu-collapsed";
+                                                                                                            } else {
+                                                                                                                echo "menu-hide";
+                                                                                                            } ?>" data-open="click" style="padding-right:0px;" data-menu="vertical-menu-modern">
+
+    <!--BEGIN: Navbar -->
+    <?php include "view/src/header.php"; ?>
+    <!--END: Navbar -->
+
+    <!--BEGIN: Sidebar -->
+    <?php include "view/src/mainMenu.php"; ?>
+    <!--END: Sidebar -->
+
+    <!-- BEGIN: Content-->
+    <div style="overflow:hidden;" class="app-content content ">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper container-xxl p-0">
+            
+            <div class="content-header row">
+            <div class="breadcrumb-wrapper">
+                            <ol class="breadcrumb ps-0">
+                                <li class="breadcrumb-item"><a href="<?=Config::BASE_URL?>dashboard">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Mural UFPR</li>
+                            </ol>
+                        </div> </div>
+            <div class="content-body">
+                <!-- Dashboard Ecommerce Starts -->
+                <h1 style="font-weight:500; ">Mural </h1>
+
+                <?php if (!isset($murais["error"])) { ?>
                             <?php foreach ($murais as $mural) { ?>
                                 <?php if ($mural["visivel"] == 1) { ?>
-                                    <div class="card white p-2">
+                                    <div class="card p-1">
                                         <h4><?= htmlspecialchars($mural["titulo"]) ?></h4>
                                         <p><?= htmlspecialchars($mural["descricao"]) ?></p>
                                         <div class="card-icons">
@@ -57,7 +111,7 @@ date_default_timezone_set('America/Sao_Paulo');
                                                     ?>
                                                 </span>
 
-                                                <a href="complete-board?id=<?= urlencode($mural["id"]) ?>" class="badge rounded-pill bg-light-secondary">
+                                                <a href="complete-board?id=<?= urlencode($mural["identificador"]) ?>" class="badge rounded-pill bg-light-secondary">
                                                     <i class="bi bi-eye"></i> Ver Detalhes
                                                 </a>
                                             </h6>
@@ -70,12 +124,41 @@ date_default_timezone_set('America/Sao_Paulo');
                                 <h4><?= $murais["message"] ?></h4>
                             </div>
                         <?php } ?>
-                    </div>
+               
 
+            </div>
+
+
+            <!-- END: Content-->
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/owl.carousel.min.js'></script>
+            <script src='https://use.fontawesome.com/826a7e3dce.js'></script>
+            <script src="src/js/swiper-bundle.min.js"></script>
+
+
+            <div class="sidenav-overlay"></div>
+            <div class="drag-target"></div>
+
+            <!-- endbuild -->
+
+            <!-- Vendors JS -->
+
+
+            <!-- Main JS -->
+            <script src="test/assets/js/main.js"></script>
+
+            <!-- <script src="layout/app-assets/js/scripts/my-department.js"></script> -->
             <?php include "view/src/footer.php"; ?>
+            <style>
+                .owl-stage-outer {
+                    height: 500px;
 
-            <script src="exemplo-de-script-1"></script>
-            <script src="exemplo-de-script-2"></script>
-        </body>
-    </html>
-    <?php endif; ?>
+                }
+
+                .cards {
+                    margin-right: 10px;
+                }
+            </style>
+</body>
+
+</html>
